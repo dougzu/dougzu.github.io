62c8758398f48e58a3399c369920a7edffb1d9b0
Douglas Zuercher <dougz@iastate.edu>
2023-01-16 22:09:26 -0600

    Added pre-push hook for pushing _site content to gh-pages branch.

---
+++ gitpublish.sh
@@ -0,0 +1,41 @@
+# Modified version of script from: https://surdu.me/2020/02/04/jekyll-git-hook.html
+
+#!/bin/sh
+
+# If any command fails in the bellow script, exit with error
+set -e
+
+# Set the name of the folder that will be created in the parent
+# folder of your repo folder, and which will temporarily
+# hold the generated content.
+temp_folder="_gh-pages-temp"
+
+# Make sure our main code runs only if we push the master branch
+if [ "$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)" == "main" ]; then
+    # Store the last commit message from main branch
+    last_message=$(git show -s --format=%s main)
+
+    # Build our Jekyll site
+    jekyll build
+
+    # Move the generated site in our temp folder
+    mv _site ../${temp_folder}
+
+    # Checkout the gh-pages branch and clean it's contents
+    git checkout gh-pages
+    rm -rf ./*
+
+    # Copy the site content from the temp folder and remove the temp folder
+    cp -r ../${temp_folder}/* .
+    rm -rf ../${temp_folder}
+
+    # Commit and push our generated site to GitHub
+    git add -A
+    git commit -m "Built \`$last_message\`"
+    git push
+
+    # Go back to the master branch
+    git checkout main
+else
+    echo "Not master branch. Skipping build"
+fi
